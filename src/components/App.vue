3ee6e6c780c1a95bbb1119b6c1d2bfebb5c5b22c
Paulo Sérgio do Nascimento <pasergio@ic.unicamp.br>
2020-05-29 22:58:46 -0300

    recuperaçao do arquivo App.vue

---
+++ src/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="success"
+      dark
+    >     
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+          <home/>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from '@/pages/home/Home'
+
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    
+  }),
+};
+</script>
